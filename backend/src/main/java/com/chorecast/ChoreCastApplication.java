38c80f0b78a9c871f51c9c6e8d945386b096ad6c
jz406zou <jz406zou@gmail.com>
2025-10-16 12:51:13 -0400

    setup springboot

---
+++ backend/src/main/java/com/chorecast/ChoreCastApplication.java
@@ -0,0 +1,13 @@
+package com.chorecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ChoreCastApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChoreCastApplication.class, args);
+    }
+}
